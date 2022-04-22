87bfce43e22b2fc43545f6334f53b96093fff8b2
Edgar David Vargas Fuentes <edgar981125@live.com.mx>
2022-04-22 15:48:35 -0500

    Agregamos nombre completo y un mensaje

--- main.cpp
+++ main.cpp
@@ -2,7 +2,13 @@
 using namespace std;
 int main()
 {
+    string nombre;
+
     cout << "Hola mundo" << endl;
+    cout << "Nombre: ";
+    getline(cin, nombre);
+    cout << "Hola " << nombre << "!" << endl;
+    cout << "Nueva modificacion!" << endl;
 
     return 0;
 }
