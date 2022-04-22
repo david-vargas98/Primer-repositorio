b85b1e120a8cc3482551739384707273b7dea54f
Edgar David Vargas Fuentes <edgar981125@live.com.mx>
2022-04-22 16:01:47 -0500

    Capturamos la edad e imprimimos un mensaje

--- main.cpp
+++ main.cpp
@@ -3,12 +3,15 @@ using namespace std;
 int main()
 {
     string nombre;
+    size_t edad;
 
     cout << "Hola mundo" << endl;
     cout << "Nombre: ";
     getline(cin, nombre);
     cout << "Hola " << nombre << "!" << endl;
-    cout << "Nueva modificacion!" << endl;
+    cout << "Ingresa tu edad:";
+    cin >> edad;
+    cout << "La edad de " << nombre << " es " << edad << " anios" << endl;
 
     return 0;
 }
